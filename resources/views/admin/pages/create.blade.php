3d782d8648eec5fd2be55517ff0dbbc2b0e190f8
Valerio Zoccante <valeriozoc@gmail.com>
2020-05-31 13:33:31 +0200

    create_view_2

--- resources/views/admin/pages/create.blade.php
+++ resources/views/admin/pages/create.blade.php
@@ -11,29 +11,29 @@
                     @method('POST')
                     <div class="form-group">
                         <label for="title">Titolo</label>
-                        <input type="text" name="title" class="form-control">
+                        <input type="text" name="title" class="form-control" value="{{old('title')}}">
                     </div>
                     <div class="form-group">
                         <label for="summary">Sommario</label>
-                        <input type="text" name="summary" class="form-control">
+                        <input type="text" name="summary" class="form-control" value="{{old('summary')}}">
                     </div>
                     <div class="form-group">
                         <label for="body">Testo</label>
-                        <textarea name="body" rows="8" cols="80" class="form-control"></textarea>
+                        <textarea name="body" rows="8" cols="80" class="form-control">{{old('body')}}</textarea>
                     </div>
                     <div class="form-group">
                         <label for="category_id">Categoria</label>
                         <select class="" name="category_id">
                             @foreach ($categories as $category)
-                                <option value="{{$category->id}}">{{$category->name}}</option>
+                                <option value="{{$category->id}}" {{(!empty(old('category_id'))) ? 'selected' : ''}}>{{$category->name}}</option>
                             @endforeach
                         </select>
                     </div>
                     <div class="form-group">
-                        @foreach ($tags as $tag)
+                        @foreach ($tags as $key => $tag)
                             <label for="tags-{{$tag->id}}">{{$tag->name}}</label>
                             {{-- deve essere in un array tags per la store, se fai dd è chiave valore --}}
-                            <input type="checkbox" value="{{$tag->id}}" name="tags[]" id="tags-{{$tag->id}}">
+                            <input type="checkbox" value="{{$tag->id}}" name="tags[]" id="tags-{{$tag->id}} {{(!empty(old('tags.'. $key))) ? 'checked' : ''}}>
                         @endforeach
                     </div>
                     <div class="form-group">
