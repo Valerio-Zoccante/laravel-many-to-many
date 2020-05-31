8cafb0da18f236fe3873f18adfda914f47644d81
Valerio Zoccante <valeriozoc@gmail.com>
2020-05-31 09:40:42 +0200

    index_view_page_2

--- resources/views/admin/pages/index.blade.php
+++ resources/views/admin/pages/index.blade.php
@@ -3,11 +3,18 @@
     <div class="container">
         <div class="row">
             <div class="col-12">
-                <table>
-                    <thead>
+                <ul class="nav justify-content-center">
+                    <li class="nav-item">
+                        <a class="nav-link active" href="{{route('admin.pages.create')}}">Aggiungi pagina</a>
+                    </li>
+                </ul>
+                <table class="table">
+                    <thead class="thead-dark">
                         <tr>
                             <th>ID</th>
                             <th>Titolo</th>
+                            <th>Categoria</th>
+                            <th>Tags</th>
                             <th>Data Creazione</th>
                             <th>Data Update</th>
                             <th colspan='3'>Azioni</th>
@@ -18,6 +25,12 @@
                             <tr>
                                 <td>{{$page->id}}</td>
                                 <td>{{$page->title}}</td>
+                                <td>{{$page->category->name}}</td>
+                                <td>
+                                    @foreach ($page->tags as $tag)
+                                        {{$tag->name}} <br>
+                                    @endforeach
+                                </td>
                                 <td>{{$page->created_at}}</td>
                                 <td>{{$page->updated_at}}</td>
                                 <td>Visualizza</td>
